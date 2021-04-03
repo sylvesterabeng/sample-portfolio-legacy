d7f29278f8dd59f6fa9598403f977b4332d830ff
Sylvester Abeng <sylvester@tagbangers.co.jp>
2021-04-03 23:59:55 +0900

    fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,6 +18,7 @@ export default class MyApp extends App {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <title>Sylvester Abeng</title>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Bellefair&family=Noto+Serif+JP:wght@300;400;500;700&display=swap"
             rel="stylesheet"
