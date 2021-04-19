0a2c25cdb747837da9d9f5b3e38453b97ad3799f
Sylvester Abeng <sylvester@tagbangers.co.jp>
2021-04-20 00:10:42 +0900

    add api

---
+++ src/data/banner-slides/index.ts
@@ -0,0 +1,34 @@
+const bannerSlides = [
+    {
+        id: '01',
+        src: 'images/garden-house.png',
+        desc: 'Garden House, Kamakura',
+    },
+    {
+        id: '02',
+        src: 'images/jomyoji.png',
+        desc: 'Jomyoji, Kamakura',
+    },
+    {
+        id: '03',
+        src: 'images/bungakukan-2.png',
+        desc: 'Kamakura Bungakukan, Kamakura',
+    },
+    {
+        id: '04',
+        src: 'images/fumotoppara.png',
+        desc: 'Fumotoppara, Shizuoka',
+    },
+    {
+        id: '05',
+        src: 'images/sakura.png',
+        desc: 'Minami Ota, Yokohama',
+    },
+    {
+        id: '06',
+        src: 'images/yamate.png',
+        desc: 'Yamate, Yokohama',
+    },
+]
+
+export default bannerSlides
---
+++ src/data/photography-slides/index.ts
@@ -0,0 +1,36 @@
+import { PhotographySlide } from '@entities/index'
+
+const photographySlides: PhotographySlide[] = [
+    {
+        name: 'kamakura-photowalk',
+        src: 'images/garden-house.png',
+        label: '鎌倉フォトウォーク',
+    },
+    {
+        name: 'jomyoji',
+        src: 'images/jomyoji.png',
+        label: '浄妙寺',
+    },
+    {
+        name: 'kamakura-bungakukan',
+        src: 'images/bungakukan.png',
+        label: '鎌倉文学館',
+    },
+    {
+        name: 'fumotoppara',
+        src: 'images/fumotoppara.png',
+        label: 'ふもとっぱら',
+    },
+    {
+        name: 'yamate',
+        src: 'images/yamate.png',
+        label: '山手',
+    },
+    {
+        name: 'sakura',
+        src: 'images/sakura.png',
+        label: '桜',
+    },
+]
+
+export default photographySlides
